aee3e8463b828f621768b3d8256b4fdfcf3efb4e
Nutsanee28 <Nutsanee.s@ku.th>
2021-02-06 23:23:12 +0700

    exercise while loop in date 6/2/2021

--- exercise_while.py
+++ exercise_while.py
@@ -3,7 +3,7 @@ while table <= 12:
     num = 1
     print("[", table, "]")
     while num <= 12:
-        print(table, " * ", num, " : ", table * num)  # พิมพ์สูตรคูณ
+        print(table, " * ", num, " : ", table * num)
         num += 1
     print("-------------")
     table += 1
